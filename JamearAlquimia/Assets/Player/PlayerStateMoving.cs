cb5d516209d24f7e68e230008ef30c779b0acffe
Gerardo Laguzzi <laguzzigerardo@gmail.com>
2022-04-25 22:10:04 -0300

    Primer prototipo

---
+++ JamearAlquimia/Assets/Player/PlayerStateMoving.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateMoving : PlayerStateBase
+{
+    Transform targetToMove;
+
+    public PlayerStateMoving(PlayerStateManager context, Transform target)
+    {
+        this.Context = context;
+        this.targetToMove = target;
+    }
+
+    public override void EnterState(){}
+    public override void ExitState(){}
+
+
+    public override void AwakeState(){}
+    public override void StartState(){}
+
+    public override void UpdateState()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 movementDirection = (targetToMove.position - Context.transform.position).normalized;
+        Context.gameObject.transform.Translate(movementDirection * Context.MovementSpeed * Time.deltaTime, Space.World);
+        CheckTargetReached();
+    }
+
+    void CheckTargetReached()
+    {
+        float diffX = Mathf.Abs(targetToMove.position.x - Context.transform.position.x);
+        if(diffX < .1f)
+        {
+            this.Context.transform.position = targetToMove.position;
+            this.Context.SetIdle();
+        }
+    }
+}
