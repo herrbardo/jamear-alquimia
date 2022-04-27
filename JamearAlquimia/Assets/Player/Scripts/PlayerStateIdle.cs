7cc82f2b0ad56b74651144b41510f0a3d56dbda7
Gerardo Laguzzi <laguzzigerardo@gmail.com>
2022-04-27 12:39:00 -0300

    GameScene arreglada y Player

---
+++ JamearAlquimia/Assets/Player/Scripts/PlayerStateIdle.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateIdle: PlayerStateBase
+{
+    public PlayerStateIdle(PlayerStateManager context)
+    {
+        this.Context = context;
+    }
+
+    public override void EnterState(){}
+    public override void ExitState(){}
+
+
+    public override void AwakeState(){}
+    public override void StartState(){}
+    public override void UpdateState(){}
+}
