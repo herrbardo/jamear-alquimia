dc54c3938049f7a82f1243e5a8c100c216c5bbc5
herrbardo <laguzzigerardo@gmail.com>
2022-04-30 16:59:44 -0300

    Caminata y coistas

--- Assets/Potion/Scripts/PotionUnlocker.cs
+++ Assets/Potion/Scripts/PotionUnlocker.cs
@@ -6,8 +6,14 @@ public class PotionUnlocker : MonoBehaviour
 {
     [SerializeField] Potions PotionToUnlock;
 
+    int count = 0;
+
     public void Unlock()
     {
+        if(count > 8)
+            return;
         GameEvents.GetInstance().OnPotionUnlocked(PotionToUnlock);
+        PotionToUnlock = PotionToUnlock + 1;
+        count++;
     }
 }
