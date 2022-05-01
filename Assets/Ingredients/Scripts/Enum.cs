a74de702a6fa74cc684e9fd9888d8bed2caaf827
herrbardo <laguzzigerardo@gmail.com>
2022-05-01 01:05:10 -0300

    Vial con tooltip y cierre automático

---
+++ Assets/Ingredients/Scripts/Enum.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Powder
+{
+    Salt,
+    Arsenic,
+    Phos
+}
+
+public enum Liquid
+{
+    Water,
+    Oil,
+    Mercury
+}
+
+public enum Element
+{
+    Gold,
+    Lead,
+    Bugs
+}
